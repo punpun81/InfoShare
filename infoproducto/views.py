1de859d6fc1b982d4fa3e363ffcf8d8bce8b9bc1
Elías Javier Menjívar Ramírez <mr0305032024@unab.edu.sv>
2024-11-04 13:47:02 -0600

    agregacion de nuevos apartados

--- infoproducto/views.py
+++ infoproducto/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+# Importamos HttpResponse
+from django.http import HttpResponse
+# Importar nuestro modelo
+from .models import Productos
+# Importar las vistas genéricas CRUD (Create, Read, Update, Delete)
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+# Importamos reverse_lazy desde Url
+from django.urls import reverse_lazy
 
-# Create your views here.
+usuario = "Admin"
+
+# Crear vista principal
+def inicio(request):
+    autenticado = False
+    
+    contexto = {
+        "esta_autenticado": autenticado,
+        "user": usuario
+    }
+    return render(request, 'pages/index.html', contexto)
+
+# Crear vistas genéricas
+
+def home(request):
+    return render(request, 'home.html')
+
+def listado(request):
+    # Obtener todos los objetos del modelo, por ejemplo, Producto
+    from .models import Producto  # Asegúrate de tener un modelo Producto
+    productos = Producto.objects.all()
+    return render(request, 'listado.html', {'productos': productos})
--- infoshare/urls.py
+++ infoshare/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('')),  # Incluir las URLs de mi_app
 ]
+
+
+# #from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('', views.home, name='home'),  # Página principal
+#     path('productos/', views.listado, name='listado'),  # Página de listado
+# ]
